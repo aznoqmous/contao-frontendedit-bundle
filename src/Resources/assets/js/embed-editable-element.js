d8e3a602c21d0a73645e99a062e3a9746fc47d8c
aznoqmous <paul.landreau931v@gmail.com>
2021-07-27 17:05:11 +0200

    working iframe

--- src/Resources/assets/js/embed-editable-element.js
+++ src/Resources/assets/js/embed-editable-element.js
@@ -25,19 +25,19 @@ export default class EmbedEditableElement {
         return type
     }
 
-    save(){
+    save() {
         let data = new FormData(this.getSettingsForm())
         fetch(this.getContentPopupUrl(), {
             method: 'POST',
             body: data
         })
-        .then(res => {
-            this.updateSettingsPane(data)
-        })
+            .then(res => {
+                this.updateSettingsPane(data)
+            })
         this.updateElement(data, true)
     }
 
-    bindElement(){
+    bindElement() {
         this.element.addEventListener('mouseenter', (e) => {
             FrontendEdit.getAllElements().map(el => el.classList.remove('hover'))
             this.element.classList.add('hover')
@@ -54,20 +54,22 @@ export default class EmbedEditableElement {
 
         let links = [...this.element.querySelectorAll('a[href]')]
         links.map(l => {
-            l.addEventListener('click', (e)=>{
+            l.addEventListener('click', (e) => {
                 e.preventDefault()
                 window.location.href = l.href
             })
         })
     }
-    fetchContentElementHtml(formData){
+
+    fetchContentElementHtml(formData) {
         return fetch(`/api/frontendedit/render/${this.id}`, {
             method: 'POST',
             body: formData
         })
             .then(res => res.json())
     }
-    updateElement(data){
+
+    updateElement(data) {
         return this.fetchContentElementHtml(data)
             .then(res => {
                 this.updateContent(res)
@@ -75,13 +77,14 @@ export default class EmbedEditableElement {
                 this.element.classList.remove('unsaved')
             })
     }
-    updateContent(html, saved=false) {
+
+    updateContent(html, saved = false) {
         let div = document.createElement('div')
         div.innerHTML = html
         let updatedElement = div.querySelector('*')
         updatedElement.classList.add('editable')
         updatedElement.classList.add('active')
-        if(!saved) updatedElement.classList.add('unsaved')
+        if (!saved) updatedElement.classList.add('unsaved')
         updatedElement.classList.add(`ce_${this.id}`)
         this.element.parentElement.insertBefore(updatedElement, this.element)
         this.element.remove()
@@ -89,61 +92,63 @@ export default class EmbedEditableElement {
         return updatedElement
     }
 
-    createSettingsPane(){
-        if(!this.settingsPane) {
-            this.settingsPane = document.createElement('div')
+    createSettingsPane() {
+        if (!this.settingsPane) {
+            this.settingsPane = document.createElement('iframe')
+            this.settingsPane.style.height = "100vh"
+            this.settingsPane.style.width = "100%"
             FrontendEdit.settingsColumn.appendChild(this.settingsPane)
             this.updateSettingsPane()
+            this.settingsPane.onload = () => {
+                this.bindSettingsPane()
+            }
         }
     }
 
-    getContentPopupUrl(){
-        if(this.type === 'tl_content') return `/contao?do=article&table=tl_content&id=${this.id}&popup=1&act=edit&rt=${FrontendEdit.rt}`
+    getContentPopupUrl() {
+        if (this.type === 'tl_content') return `/contao?do=article&table=tl_content&id=${this.id}&popup=1&act=edit&rt=${FrontendEdit.rt}`
         return null
     }
 
-    getAPIContentPopupUrl(){
-        if(this.type === 'tl_content') return `/api/frontendedit/form/${this.id}`
+    getAPIContentPopupUrl() {
+        if (this.type === 'tl_content') return `/api/frontendedit/form/${this.id}`
         return null
     }
 
-    updateSettingsPane(data=new FormData()) {
-        return fetch(this.getAPIContentPopupUrl(), {
-            method: 'POST',
-            body: data
+    bindSettingsPane() {
+        let sfToolBar = this.settingsPane.contentDocument.querySelector('.sf-toolbar')
+        if(sfToolBar) sfToolBar.remove()
+        let splitButtons = this.settingsPane.contentDocument.querySelector('.split-button')
+        if(splitButtons) splitButtons.parentElement.removeChild(splitButtons)
+        let tlBoxes = [...this.settingsPane.contentDocument.querySelectorAll('tl_box')]
+        tlBoxes.map(b => b.classList.add('collapsed'))
+        this.bindIframe(this.settingsPane)
+    }
+
+    bindIframe(iframe) {
+        let forms = [...iframe.contentDocument.querySelectorAll('form')]
+        forms.map(form => {
+            this.updateElement(new FormData(this.getSettingsForm()), false)
+            let fields = [...form.querySelectorAll('input,select,textarea,checkbox,radio')]
+            fields.map(f => f.addEventListener('change', () => {
+                let data = new FormData(this.getSettingsForm())
+                this.updateElement(data, false)
+                this.bindSettingsPane()
+            }))
         })
-            .then(res => res.text())
-            .then(html => {
-                this.settingsPane.innerHTML = html
-                setTimeout(()=>{
-                    let splitButtons = this.settingsPane.querySelector('.split-button')
-                    splitButtons.parentElement.removeChild(splitButtons)
-                    let tlBoxes = [...this.settingsPane.querySelectorAll('tl_box')]
-                    tlBoxes.map(b => b.classList.add('collapsed'))
-                    let form = this.getSettingsForm()
-                    this.settingsPane.innerHTML = ''
-                    this.settingsPane.appendChild(form)
-                    form.addEventListener('submit', (e)=>{
-                        e.preventDefault()
-                        this.save()
-                    })
-                    let fields = [...form.querySelectorAll('input,select,textarea,checkbox,radio')]
-                    fields.map(f => f.addEventListener('change', ()=>{
-                        let data = new FormData(this.getSettingsForm())
-                        this.updateElement(data, false)
-                        this.updateSettingsPane(data)
-                    }))
-                    FrontendEdit.reloadBackendScripts()
-                }, 10)
-            })
     }
-    openSettingsPane(){
-        if(!this.settingsPane) this.createSettingsPane()
+
+    updateSettingsPane(data = new FormData()) {
+        this.settingsPane.src = this.getContentPopupUrl()
+    }
+
+    openSettingsPane() {
+        if (!this.settingsPane) this.createSettingsPane()
         FrontendEdit.closeAllSettingsPane()
         this.settingsPane.style.display = 'block'
     }
 
-    getSettingsForm(){
-        return this.settingsPane ? this.settingsPane.querySelector('form#tl_content') : null
+    getSettingsForm() {
+        return this.settingsPane ? this.settingsPane.contentDocument.querySelector('form#tl_content') : null
     }
 }
