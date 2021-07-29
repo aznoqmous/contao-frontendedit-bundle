3a0c18481c0ddcd2bc006f2574bca81ce224e03f
aznoqmous <paul.landreau931v@gmail.com>
2021-07-29 15:14:03 +0200

    filters forms (ce_form) from ParseTemplateListener

--- src/EventListener/ParseTemplateListener.php
+++ src/EventListener/ParseTemplateListener.php
@@ -12,11 +12,16 @@ class ParseTemplateListener
 {
     public function __invoke($template): void
     {
-        if($template->typePrefix == 'ce_' && !$template->hookModified) {
+        if($template->hookModified) return;
+        if(
+            $template->typePrefix == 'ce_'
+            && $template->type
+        ) {
+
             $template->class .= " editable ce_{$template->id}";
             $template->hookModified = true;
         }
-        if($template->typePrefix == 'mod_' && !$template->hookModified) {
+        if($template->typePrefix == 'mod_') {
             $template->class .= " editable mod_{$template->id}";
             $template->hookModified = true;
         }
